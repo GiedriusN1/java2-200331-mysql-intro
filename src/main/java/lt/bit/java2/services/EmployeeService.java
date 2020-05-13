dc6824488c83fb510fd2b213f362dd7327738dcd
GiedriusNekrasas <giedrius.nekrasas@gmail.com>
2020-05-13 20:05:27 +0300

    TODO: ideti salaries pasikartojantiems emp_no

--- src/main/java/lt/bit/java2/services/EmployeeService.java
+++ src/main/java/lt/bit/java2/services/EmployeeService.java
@@ -23,68 +23,64 @@ public class EmployeeService {
     public static List<Employee> loadEmployees(int pageNo, int pageSize) {
         List<Employee> employeeList = new ArrayList<>();
         ResultSet resultSet;
+        ResultSet rs;
 
         try (Connection connection = DBService.getConnectionFromCP()) {
 
-            String sql = "SELECT * FROM employees_test WHERE LIMIT ?, ?;";
+            String sql = "SELECT * FROM employees_test LIMIT ?, ?;";
 
             PreparedStatement prepareStatement = connection.prepareStatement(sql);
             prepareStatement.setInt(1, pageSize * pageNo);
             prepareStatement.setInt(2, pageSize);
 
             resultSet = prepareStatement.executeQuery();
-
-            int lastEmpNo = 0;
-            //           pasiziurim ka gavom resultSet
+            int i = 0;
+//            int lastEmpNo = 0;
+//            //           pasiziurim ka gavom resultSet
             while (resultSet.next()) {
-                System.out.println(" -> " +
+                i++;
+                System.out.println(" -> " + i + " ciklas, emp_no  " +
                         resultSet.getInt("emp_no") + " " +
                         resultSet.getString("first_name") + " " +
                         resultSet.getString("last_name") + " " +
                         resultSet.getString("gender") + " " +
                         resultSet.getDate("birth_date") + " " +
-                        resultSet.getDate("hire_date") + " " +
-                        resultSet.getInt("salary") + " " +
-                        resultSet.getDate("to_date") + " " +
-                        resultSet.getDate("from_date")
+                        resultSet.getDate("hire_date")
                 );
 
+                // gaunam einamojo employee emp_no pagal kuri darysim selekta is salaries_test
                 int currEmpNo = resultSet.getInt("emp_no");
+
                 Employee currentEmployee; // = null;
-                Salary salary; // = null;
-
                 currentEmployee = EmployeeMap.fromResultSet(resultSet);
+                employeeList.add(currentEmployee);
 
+                Salary salary; // = null;
+                String sal = "SELECT * FROM salaries_test WHERE emp_no = ?;";
+                PreparedStatement prep = connection.prepareStatement(sal);
+                prep.setInt(1, currEmpNo);
+                rs = prep.executeQuery();
                 List<Salary> algeles = new ArrayList<>();
-                salary = SalaryMap.fromResultSet(resultSet);
-                salary.setSalary(currentEmployee.getEmpNo());
-                algeles.add(salary);
 
-                // ziurim ar tai naujas EmpNo
-                if (currEmpNo != lastEmpNo) {
+                while (rs.next()) {
 
-                    System.out.println(
-                            "Naujo empNo Salary duomenys: " +
-                                    salary.getSalary() + " " +
-                                    salary.getEmpNo() + " " +
-                                    salary.getFromDate() + " " +
-                                    salary.getToDate()
+                    System.out.println(" Salary -> " +
+                            rs.getInt("emp_no") + " " +
+                            rs.getDate("from_date") + " " +
+                            rs.getDate("to_date") + " " +
+                            rs.getInt("salary")
                     );
 
-                    currentEmployee.setSalaries(algeles);
-                    employeeList.add(currentEmployee);
-
-                } else {
-                    /** TODO jei empNo toks pats kaip pries tai buves, pridedam tik salary i employee lista salary.
-                     TODO cia reiktu burtazodzio kad  Employee liste  Salaries ideti papildoma salary lista
-                     **/
+                    Salary currentSalary;
+                    currentSalary = SalaryMap.fromResultSet(rs);
+                    algeles.add(currentSalary);
                 }
-                lastEmpNo = currEmpNo;
+                currentEmployee.setSalaries(algeles);
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println("EmployeeService.employeeList.size  " + employeeList.size());
         return employeeList;
     }
 }
